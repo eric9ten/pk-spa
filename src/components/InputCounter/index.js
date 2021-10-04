496cfadce3872e2b3bf0d26075b2c7f1b1f48a04
elarsenBoz <53196036+elarsenBoz@users.noreply.github.com>
2021-10-03 23:07:53 -0500

    Added fields to stat form

--- src/components/InputCounter/index.js
+++ src/components/InputCounter/index.js
@@ -5,6 +5,8 @@ import s from './input-counter.module.scss'
 
 export default function InputCounter(props) {
     
+
+	const inputSide = props.inputSide === 'right' ? s.inputRight : s.inputLeft
     const [count, setCount] = React.useState( 0 );
 
     function incrementCount(e) {
@@ -16,10 +18,10 @@ export default function InputCounter(props) {
     }
 
     return (
-        <div className={s.inputCounter}>
-            <input type="button" value="-" className={`${s.counterButton} ${s.buttonMinus}`} onClick={decrementCount}/>
-            <input type="number" step="1" max={props.maxInc} value={count} name={props.inputName} size={props.size} className={s.input} />
+        <div className={ `${s.inputCounter} ${inputSide}` }>
+            <input type="button" value="-" className={` ${s.counterButton} ${s.buttonMinus} `} onClick={decrementCount}/>
             <input type="button" value="+" className={`${s.counterButton} ${s.buttonPlus}`} onClick={incrementCount}/>
+            <input type="number" step="1" max={props.maxInc} value={count} name={props.inputName} size={props.size} className={s.input} />
         </div>
     )
 }
--- src/components/StatTrackingForm/index.js
+++ src/components/StatTrackingForm/index.js
@@ -7,14 +7,104 @@ export default function StatTrackingForm() {
 
     return (
         <div className={s.statTrackingForm}>
-            <div className={s.leftGoal}>
-                <Counter inputName={"leftPasses"} maxInc={999} size={4} />
+            <div className={`${s.statGroup} ${s.passes}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftPasses"} maxInc={999} size={4} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Passes</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightPasses"} maxInc={999} size={4} inputSide={"left"} />
+                </div>
             </div>
-            <div className={s.labels}>
-                <p>Passes</p>
+            <div className={`${s.statGroup} ${s.shots}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftShots"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Shots</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightShots"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
             </div>
-            <div className={s.rightGoal}>
-                <Counter inputName={"rightPasses"} maxInc={999} size={4} />
+            <div className={`${s.statGroup} ${s.corners}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftCorners"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Corners</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightCorners"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
+            </div>
+            <div className={`${s.statGroup} ${s.goalKicks}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftGoalKicks"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Goal Kicks</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightGoalKicks"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
+            </div>
+            <div className={`${s.statGroup} ${s.tackles}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftTackles"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Tackles</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightTackles"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
+            </div>
+            <div className={`${s.statGroup} ${s.offsides}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftOffsides"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Offsides</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightOffsides"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
+            </div>
+            <div className={`${s.statGroup} ${s.fouls}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftFouls"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Fouls</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightFouls"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
+            </div>
+            <div className={`${s.statGroup} ${s.yellowCards}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftYellowCards"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Yellow Cards</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightYellowCards"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
+            </div>
+            <div className={`${s.statGroup} ${s.redCards}`}>
+                <div className={`${s.leftGoal} ${s.statCol}`}>
+                    <Counter inputName={"leftRedCards"} maxInc={99} size={3} inputSide={"right"} />
+                </div>
+                <div className={s.labels}>
+                    <p>Red Cards</p>
+                </div>
+                <div className={`${s.rightGoal} ${s.statCol}`}>
+                    <Counter inputName={"rightRedCards"} maxInc={99} size={3} inputSide={"left"} />
+                </div>
             </div>
         </div>
     )
