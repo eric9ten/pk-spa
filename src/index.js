7be47f8818af7de6321455fa0e8435894a36ae14
eric9ten <67991762+eric9ten@users.noreply.github.com>
2022-01-11 14:40:16 -0600

    Added service worker for PWA

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import store from './app/store'
-import App from './App';  
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import Routes from './Routes';
 
@@ -22,6 +23,11 @@ ReactDOM.render(
   document.getElementById('root')
 
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
